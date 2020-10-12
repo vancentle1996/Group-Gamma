7236c2883f56e3f9511c132715702e70c82bf13b
TLemire065 <tlemire065@g.rwu.edu>
2020-10-12 18:11:43 -0400

    Modal Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from './comps/Modal';
 import Title from './comps/Title';
+import ImageGrid from './comps/ImageGrid';
 
 function App() {
+
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="App">
       <Title/>
+      <ImageGrid setSelectedImg ={setSelectedImg} />
+  { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg}/> }
     </div>
   );
 }
